556671f40eeedc76b7df00fd4f6c811073217635
mshcherbina <marinashcherbina89@gmail.com>
2022-12-30 20:27:21 +0300

    add greeting

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const greeting = () => {
+    const gamerName = readlineSync.question('May I have your name?')
+    console.log('Hello, ' + gamerName + '!');
+}
+
+export default greeting;
