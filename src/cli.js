758070511782113b0c778ad600ded503f97bc1bc
mshcherbina <marinashcherbina89@gmail.com>
2022-12-30 23:13:29 +0300

    correct syntax errors

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
-    const gamerName = readlineSync.question('May I have your name?')
-    console.log('Hello, ' + gamerName + '!');
-}
+  const gamerName = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${gamerName}!`);
+};
 
 export default greeting;
