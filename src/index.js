b8e1e2f8f871ad7fe3d33b1481a8c62e28dbebab
mshcherbina <marinashcherbina89@gmail.com>
2023-01-03 15:07:49 +0300

    Fix task field and rename constant

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import readlineSync from 'readline-sync';
 
 const startGame = (task, randomQuest, correctAnswer) => {
   console.log('Welcome to the Brain Games!');
-  const gamerName = readlineSync.question('May I have your name?');
+  const gamerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${gamerName}!`);
-  console.log(`${task()}`);
+  task();
 
   for (let i = 0; i < 3; i += 1) {
     const question = randomQuest();
     console.log(`Question: ${question}`);
-    const rightAnswer = correctAnswer(question);
+    const expectedAnswer = correctAnswer(question);
     const gamerAnswer = readlineSync.question('Your answer: ');
-    if (gamerAnswer.toString() === rightAnswer.toString()) {
+    if (gamerAnswer.toString() === expectedAnswer.toString()) {
       console.log('Correct!');
     } else {
-      console.log(`'${gamerAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+      console.log(`'${gamerAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.`);
       console.log(`Let's try again, ${gamerName}!`);
       return;
     }
