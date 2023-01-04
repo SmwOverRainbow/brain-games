e6c6cc067d7d882b59cf6bbb7c4f3a2dd2a1dac3
mshcherbina <marinashcherbina89@gmail.com>
2023-01-04 14:50:22 +0300

    Fix bug in src/games/brain-progression.js

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -7,8 +7,8 @@ const task = () => {
 
 const getProgression = () => {
   const stepProgr = _.random(1, 5);
-  const lengthProgr = _.random(9, 10);
-  const firstNumProgr = _.random(0, 10);
+  const lengthProgr = 10;
+  const firstNumProgr = _.random(0, 9);
   let progression = '';
   let nextNum = firstNumProgr;
   for (let i = 0; i <= lengthProgr; i += 1) {
@@ -19,7 +19,7 @@ const getProgression = () => {
 };
 
 const randomQuest = () => {
-  const positionOfMissingNum = _.random(0, 8);
+  const positionOfMissingNum = _.random(0, 9);
   const string = getProgression();
   const numbers = string.split(' ');
   numbers[positionOfMissingNum] = '..';
@@ -27,13 +27,13 @@ const randomQuest = () => {
 };
 
 const correctAnswer = (string) => {
-  const members = string.split(' ');
+  const members = string.trim().split(' ');
   const indexOfMissingNum = members.indexOf('..');
   let missingNum = 0;
   let searchStep = 0;
-  if ((indexOfMissingNum === members.length - 1) || (indexOfMissingNum === members.length - 2)) {
+  if (indexOfMissingNum === members.length - 1 || indexOfMissingNum === members.length - 2) {
     searchStep = members[indexOfMissingNum - 1] - members[indexOfMissingNum - 2];
-    missingNum = searchStep + members[indexOfMissingNum - 1];
+    missingNum = searchStep + Number(members[indexOfMissingNum - 1]);
   } else {
     searchStep = members[indexOfMissingNum + 2] - members[indexOfMissingNum + 1];
     missingNum = members[indexOfMissingNum + 1] - searchStep;
