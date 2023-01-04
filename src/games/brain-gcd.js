dda4f998e467d991f8e7a87f8e404896707d1ba0
mshcherbina <marinashcherbina89@gmail.com>
2023-01-05 01:42:11 +0300

    Delete code in comments in src/games/brain-gcd.js

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -23,29 +23,6 @@ const getRoundData = () => {
   return [randomQuest, correctAnswer];
 };
 
-/* const getRandomQuest = () => {
-  const randomNumber1 = _.random(1, 20);
-  const randomNumber2 = _.random(1, 20);
-  return `${randomNumber1} ${randomNumber2}`;
-};
-
-const getCorrectAnswer = (string) => {
-  const members = string.split(' ');
-  const num1 = Number(members[0]);
-  const num2 = Number(members[1]);
-  const smallestNum = Math.min(num1, num2);
-  let nod = smallestNum;
-
-  for (let i = smallestNum; i > 0; i -= 1) {
-    if ((num1 % nod === 0) && (num2 % nod === 0)) {
-      return nod;
-    }
-    nod -= 1;
-  }
-
-  return nod;
-}; */
-
 const startBrainGcd = () => {
   startGame(task, getRoundData);
 };
